d7e8340bffcf680211d1321d62e45d91ded26b76
Albert G <albert.grekowski@gmail.com>
2023-10-23 13:47:59 +0200

    enable CORS for user-auth

--- backend/src/main/java/com/foodies/backend/security/demo/DemoController.java
+++ backend/src/main/java/com/foodies/backend/security/demo/DemoController.java
@@ -1,11 +1,13 @@
 package com.foodies.backend.security.demo;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = {"http://localhost:5173", "http://13.51.176.163:5173"})
 @RequestMapping("/api/v1/demo-controller")
 public class DemoController {
    @GetMapping
